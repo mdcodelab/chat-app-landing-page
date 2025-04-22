4229524fe0551ec7340ae71df4c3b0459f59ed3e
miha11-hash <d_mihaela@msn.com>
2025-04-23 00:54:50 +0300

    Testimonials - main style

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './Testimonials.css';
 
 const Testimonials = () => {
+
   return (
     <section id="testimonials">
-        <h2 className="testimonials-title">Testimonials</h2>
-        <div className="hexagon-container">
-      <div className="testimonial center"></div>
-      <div className="testimonial top-left">Sus Stânga</div>
-      <div className="testimonial top-right">Sus Dreapta</div>
-      <div className="testimonial left">Stânga</div>
-      <div className="testimonial right">Dreapta</div>
-      <div className="testimonial bottom-left">Jos Stânga</div>
-      <div className="testimonial bottom-right">Jos Dreapta</div>
-        </div>
+      <h2 className="testimonials-title">Testimonials</h2>
+      <div className="hexagon-container">
+        <div className="testimonial center"></div>
+
+        <div className="testimonial top-left" data-aos="fade-down" data-aos-delay="100"></div>
+        <div className="testimonial top-right" data-aos="fade-down" data-aos-delay="200"></div>
+        <div className="testimonial left" data-aos="fade-left" data-aos-delay="300"></div>
+        <div className="testimonial right" data-aos="fade-right" data-aos-delay="400"></div>
+        <div className="testimonial bottom-left" data-aos="fade-up" data-aos-delay="500"></div>
+        <div className="testimonial bottom-right" data-aos="fade-up" data-aos-delay="600"></div>
+      </div>
     </section>
   );
 };
